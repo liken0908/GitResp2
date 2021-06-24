857dd4c7a36f189b4227bdfe06124190af2cd587
liken <345359675@qq.com>
2021-06-24 20:37:00 +0800

    在Person类增加了 name 属性

---
+++ TestModule01/src/com/liken/Person.java
@@ -0,0 +1,11 @@
+package com.liken;
+
+/**
+ * @Auther: Liken
+ * @Date: 2021/6/24 - 06 - 24 - 20:36
+ * @Description: com.liken
+ * @version: 1.0
+ */
+public class Person {
+    private String name;
+}
